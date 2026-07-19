f4d677aa8daeaab0976b058d80427b825b8abea7
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-07-19 22:01:49 +0530

    fix(make): You can now use `make` to also compile
    This is if you do not want `ninja`
    
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- cmake/WaylandScanner.cmake
+++ cmake/WaylandScanner.cmake
@@ -4,22 +4,29 @@ if(NOT WAYLAND_SCANNER_EXECUTABLE)
 endif()
 
 function(wayland_generate_protocol xml_path protocol_name)
-  set(out_header "${CMAKE_BINARY_DIR}/protocol/${protocol_name}-client-protocol.h")
-  set(out_source "${CMAKE_BINARY_DIR}/protocol/${protocol_name}-client-protocol.c")
+  set(output_dir "${CMAKE_BINARY_DIR}/protocol")
+
+  file(MAKE_DIRECTORY "${output_dir}")
+
+  set(out_header "${output_dir}/${protocol_name}-client-protocol.h")
+  set(out_source "${output_dir}/${protocol_name}-client-protocol.c")
 
   add_custom_command(
-    OUTPUT ${out_header}
-    COMMAND ${WAYLAND_SCANNER_EXECUTABLE} client-header ${xml_path} ${out_header}
-    DEPENDS ${xml_path}
+    OUTPUT "${out_header}"
+    COMMAND "${WAYLAND_SCANNER_EXECUTABLE}" client-header "${xml_path}" "${out_header}"
+    DEPENDS "${xml_path}"
     COMMENT "Generating ${protocol_name} client header"
-  )
-  add_custom_command(
-    OUTPUT ${out_source}
-    COMMAND ${WAYLAND_SCANNER_EXECUTABLE} private-code ${xml_path} ${out_source}
-    DEPENDS ${xml_path}
-    COMMENT "Generating ${protocol_name} private code"
+    VERBATIM
   )
 
-  set(${protocol_name}_HEADER ${out_header} PARENT_SCOPE)
-  set(${protocol_name}_SOURCE ${out_source} PARENT_SCOPE)
+  add_custom_command(
+    OUTPUT "${out_source}"
+    COMMAND "${WAYLAND_SCANNER_EXECUTABLE}" private-code "${xml_path}" "${out_source}"
+    DEPENDS "${xml_path}"
+    COMMENT "Generating ${protocol_name} private code"
+    VERBATIM
+  )
+
+  set(${protocol_name}_HEADER "${out_header}" PARENT_SCOPE)
+  set(${protocol_name}_SOURCE "${out_source}" PARENT_SCOPE)
 endfunction()
