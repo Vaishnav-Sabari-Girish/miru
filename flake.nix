265aa0e4b8d325b6cb681147aa65a931fc1388a8
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-07-24 12:09:40 +0530

    feat(nix): Tested flake.nix
    It works
    
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- flake.nix
+++ flake.nix
@@ -7,46 +7,63 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-        devShells.default = pkgs.mkShell {
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # Define the derivation once so we can reference it in 'packages' and 'apps'
+        miru = pkgs.stdenv.mkDerivation {
+          pname = "miru";
+          version = "0.2.0";
+          src = ./.;
+
           nativeBuildInputs = with pkgs; [
-                cmake
-                ninja
-                pkg-config
-                wayland-scanner
+            cmake
+            ninja
+            pkg-config
+            wayland-scanner
           ];
 
           buildInputs = with pkgs; [
-                wayland
-                wayland-protocols
+            wayland
+            wayland-protocols
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+            wayland-scanner
           ];
 
-              shellHook = ''
-                echo Entering Nix Dev Shell
-                '';
+          buildInputs = with pkgs; [
+            wayland
+            wayland-protocols
+          ];
+
+          shellHook = ''
+            echo Entering Nix Dev Shell
+          '';
+        };
+
+        packages.default = miru;
+
+        # Defines default binary for 'nix run'
+        apps = {
+          default = {
+            type = "app";
+            program = "${miru}/bin/miru-daemon";
           };
-
-          packages.default = pkgs.stdenv.mkDerivation {
-              pname = "miru";
-              version = "0.2.0";
-              src = ./.;
-
-              nativeBuildInputs = with pkgs; [
-                  cmake
-                  ninja
-                  pkg-config
-                  wayland-scanner
-              ];
-              buildInputs = with pkgs; [
-                  wayland
-                  wayland-protocols
-              ];
+          
+          # Optional: allows running 'nix run .#miructl -- toggle'
+          miructl = {
+            type = "app";
+            program = "${miru}/bin/miructl";
           };
-    }
-  );
-
+        };
+      }
+    );
 }
