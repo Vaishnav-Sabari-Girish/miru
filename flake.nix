27552e82636d37318d00cf594d29e2716a7442cf
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-09-22 10:53:43 +0530

    chore(flake): update flake.nix [skip ci]
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
         miru = pkgs.stdenv.mkDerivation {
           pname = "miru";
-          version = "0.9.0";
+          version = "0.10.0";
           src = ./.;
 
           nativeBuildInputs = with pkgs; [
